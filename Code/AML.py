02f2c5f24991eaf643f88f2eef87ac2e08aec775
tomlst <107504272+tomlst@users.noreply.github.com>
2022-11-29 22:39:17 -0500

    Add files via upload

--- Code/AML.py
+++ Code/AML.py
@@ -10,6 +10,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
 
 
 df = pd.read_excel(r"C:\Users\tomls\OneDrive\桌面\AML\AML and ATF Modelling Assignment data_std.xlsx")
@@ -22,11 +23,6 @@ X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2)
 
 sc = StandardScaler()
 
-regressor = RandomForestRegressor(n_estimators=1000, random_state=0)
-regressor.fit(X_train, y_train)
-y_pred = regressor.predict(X_test)
-print(y_pred)
-
 
 import time
 import numpy as np
@@ -35,7 +31,7 @@ import seaborn as sns
 
 X = df
 feature_names = [f"feature {i}" for i in range(X.shape[1])]
-forest = RandomForestClassifier(random_state=0)
+forest = RandomForestClassifier(n_estimators=1000,random_state=0)
 forest.fit(X_train, y_train)
 start_time = time.time()
 importances = forest.feature_importances_
@@ -45,7 +41,7 @@ fi_df = pd.DataFrame(importances,index=df.columns)
 fi_df=fi_df.sort_values(by=0,ascending=False)
 fi_df = fi_df[:20]
 #selected_features = fi_df.index.to_numpy()
-sns.heatmap(df[fi_df.index].corr())
+
 
 print(f"Elapsed time to compute the importances: {elapsed_time:.3f} seconds")
 
@@ -56,12 +52,70 @@ from h2o.automl import H2OAutoML
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.metrics import accuracy_score
 
+selected_features = ['cust_tenure', 'prod_total_balance', 'in_person_visit_cnt',
+       'avg_cash_5m', 'txn_cash_in_avg_5m', 'prod_asset_balance',
+       'avg_wire_12m', 'txn_cash_in_val_5m', 'tot_cash_val_5m',
+       'prod_primary_crc_cnt', 'txn_cash_out_avg_5m',
+       'prod_primary_cda_cnt', 'prod_prim_acct_cnt']
+
+X = df
+feature_names = [f"feature {i}" for i in range(X.shape[1])]
+forest = RandomForestClassifier(n_estimators=1000,random_state=0)
 
 X_train, X_test, y_train, y_test = train_test_split(df[selected_features], y, test_size=0.2)
+forest.fit(X_train, y_train)
 
 sc = StandardScaler()
 
-regressor = RandomForestRegressor(n_estimators=1000, random_state=0)
-regressor.fit(X_train, y_train)
-y_pred = regressor.predict(X_test)
+y_pred = forest.predict(X_test[selected_features])
+sns.heatmap(df[selected_features].corr())
 print(y_pred)
+
+df_train = X_train
+df_train["rating"] = y_train
+
+df_test = X_test
+df_test["rating"] = y_test
+
+# convert pandas DataFrame into H2O Frame
+train_h2o = h2o.H2OFrame(df_train)
+# Describe  the train h20Frame
+train_h2o.describe()
+
+# load test dataset
+test_h2o = h2o.H2OFrame(df_test)
+# selecting feature and label columns
+ 
+x = test_h2o.columns
+x.remove("rating")
+y = 'rating'
+
+# Describe  the train h20Frame
+test_h2o.describe()
+
+# callh20automl  function
+aml = H2OAutoML(max_runtime_secs = 300,
+                # max_models =25
+                # exclude_algos =['DeepLearning'],
+                seed = 1,
+                # stopping_metric ='logloss',
+                sort_metric ='RMSE',
+                balance_classes = False
+)
+# train model and record time % time
+aml.train(x = x, y = y, training_frame = train_h2o)
+
+best_model = aml.get_best_model()
+print(best_model)
+
+# Get the metalearner model of top model
+metalearner = h2o.get_model(best_model.metalearner()['name'])
+ 
+# list baselearner models :
+metalearner.varimp()
+
+predicted_result = best_model.predict(test_h2o)
+predicted_result = predicted_result.as_data_frame()
+predicted_result.index.name = 'id'
+predicted_result.rename(columns = {'predict':'price'}, inplace = True)
+predicted_result
